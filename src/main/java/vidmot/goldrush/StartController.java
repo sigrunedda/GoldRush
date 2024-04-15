159b19250a933815ee43ae77834728406a8207db
Kristín Fríða Sigurborgardóttir <143341930+KristinFrida@users.noreply.github.com>
2024-04-15 23:24:00 +0000

    Update StartController.java

--- src/main/java/vidmot/goldrush/StartController.java
+++ src/main/java/vidmot/goldrush/StartController.java
@@ -7,21 +7,30 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
-
+/**
+ * Klasinn sem sér um heimasíðu leiksins eða start-view.fxml
+ */
 public class StartController {
-
+    /**
+     * Ef að Hefja leik er valið, þá er fært notandann í valmyndina fyrir erfiðleikann
+     */
     @FXML
     private void onHefjaLeik(){
         System.out.println("Hefja Leik!");
         ViewSwitcher.switchTo(View.ERFIDLEIKI);
     }
+    /**
+     * Ef að Leikreglur er valið þá er breytt yfir í leikreglur-view.fxml og notandinn getur lesið um leikreglur
+     */
 
     @FXML
     private void onLeikreglur(){
         System.out.println("Leikreglur display!");
         ViewSwitcher.switchTo(View.LEIKREGLUR);
     }
-
+    /**
+     * Ef að Hætta er valið þá birtist alert sem býður notandanum að loka forritinu
+     */
     @FXML
     private void onHaettaLeik(){
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
